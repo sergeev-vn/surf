ea05f684a17e24d74dfb68e107a1f5f4e6b52ff6
Vladimir <sergeev24085@mail.ru>
2020-04-27 22:50:31 +0700

    add arrows to slider travel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -34,7 +34,9 @@ $(function(){
   $('.slider-travel').slick({
     slidesToShow: 1,
     infinite: true,
-    fade: true
+    fade: true,
+    prevArrow: $('.slider-travel__slider-btns .slider-btn--prev'),
+    nextArrow: $('.slider-travel__slider-btns .slider-btn--next'),
   });
   
 
